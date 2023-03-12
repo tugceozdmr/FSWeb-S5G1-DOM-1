61256d382d27f9442a56cea55d12ea3b2bbec480
tugce <tugceozdemiir@hotmail.com>
2023-03-12 17:00:28 +0300

    tugce

--- src/index.js
+++ src/index.js
@@ -41,5 +41,56 @@ const siteContent = { // BU NESNEYİ DEĞİŞTİRMEYİN
 
 console.log('Proje açıldı!')
 
+const navigationItems=document.querySelectorAll("nav a");
+var i=1;
+navigationItems.forEach((a)=>{
+  a.textContent=siteContent["nav"]["nav-item-"+i];
+  a.setAttribute("class","italic");
+i++;
+})
 
-/* Kodlar Buradan aşağıya */
+const logoImage=document.getElementById("logo-img");
+logoImage.src=siteContent["images"]["logo-img"];
+
+var mainText=document.querySelector(".cta-text");
+mainText.querySelector("h1").textContent=siteContent["cta"]["h1"];
+mainText.querySelector("button").textContent=siteContent["cta"]["button"];
+
+document.getElementById('cta-img').src=siteContent.images['cta-img'];
+document.getElementById('cta-img').alt="Image of a code snippet";
+
+
+document.querySelectorAll(".top-content .text-content h4")[0].textContent=siteContent["ana-içerik"]["özellikler-h4"];
+document.querySelectorAll(".top-content .text-content h4")[1].textContent=siteContent["ana-içerik"]["hakkımızda-h4"];
+
+document.querySelectorAll(".top-content .text-content p")[0].textContent=siteContent["ana-içerik"]["özellikler-içerik"];
+document.querySelectorAll(" .top-content .text-content p")[1].textContent=siteContent["ana-içerik"]["hakkımızda-içerik"];
+
+const image2=document.querySelector("#middle-img");
+image2.setAttribute("src",siteContent.images["accent-img"]);
+image2.setAttribute("alt","Bir kod parçası resmi")
+
+document.querySelectorAll(" .bottom-content .text-content h4")[0].textContent=siteContent["ana-içerik"]["servisler-h4"];
+document.querySelectorAll(" .bottom-content .text-content p")[0].textContent=siteContent["ana-içerik"]["servisler-içeriği"];
+
+document.querySelectorAll(" .bottom-content .text-content h4")[1].textContent=siteContent["ana-içerik"]["ürünler-h4"];
+document.querySelectorAll(" .bottom-content .text-content p")[1].textContent=siteContent["ana-içerik"]["ürünler-içeriği"];
+
+document.querySelectorAll(" .bottom-content .text-content h4")[2].textContent=siteContent["ana-içerik"]["vizyon-h4"];
+document.querySelectorAll(" .bottom-content .text-content p")[2].textContent=siteContent["ana-içerik"]["vizyon-içeriği"];
+
+const iletisimText = document.querySelectorAll("h4");
+iletisimText[5].textContent = siteContent.iletisim["iletişim-h4"];
+
+const iletisimBilgileri =document.querySelectorAll("p");
+iletisimBilgileri[5].textContent = siteContent.iletisim.adres;
+iletisimBilgileri[6].textContent = siteContent.iletisim.telefon;
+iletisimBilgileri[7].textContent = siteContent.iletisim.email;
+
+const footerText=document.querySelector("footer a");
+footerText.setAttribute("class","bold");
+footerText.textContent=siteContent["footer"]["copyright"];
+
+
+/* Kodlar Buradan aşağıya
+document.getElementById('cta-img').setAttribute('src',siteContent.images['cta-img']); */
